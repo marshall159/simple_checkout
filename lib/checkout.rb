5576ed5172eb78c1e45e6a37711dd28de7ac9bf7
Aneel Marshall <aneel.marshall@outlook.com>
2018-11-22 17:15:29 +0000

    feat: add Checkout#total to return total price of all scanned items

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -9,4 +9,8 @@ class Checkout
         items << item 
         item 
     end
+
+    def total
+        items.reduce(0) { |sum, item| sum + item.price }
+    end
 end
--- spec/unit/checkout_spec.rb
+++ spec/unit/checkout_spec.rb
@@ -18,11 +18,23 @@ describe Checkout do
         it "should initially be empty" do
             expect(checkout.items).to be_empty
         end
-        
+
         it "stores scanned items" do
             checkout.scan(item)
 
             expect(checkout.items).to include(item)
+        end
+    end
+
+    describe "#total" do
+        it "adds the total price of all scanned items" do
+            allow(item).to receive(:price).and_return(100)
+
+            checkout.scan(item)
+            checkout.scan(item)
+            checkout.scan(item)
+
+            expect(checkout.total).to eq(300)
         end
     end
 end
